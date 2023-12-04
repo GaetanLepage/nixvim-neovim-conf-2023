f04461bb1c4d7dcabb80fc876bf9ecbf6724074b
Gaetan Lepage <gaetan@glepage.com>
2023-12-04 14:05:04 +0100

    slides

--- shell.nix
+++ shell.nix
@@ -3,6 +3,25 @@ pkgs.mkShell {
   # buildInputs is for dependencies you'd need "at run time",
   # were you to to use nix-build not nix-shell and build whatever you were working on
   buildInputs = with pkgs; [
-    slides
+    (rustPlatform.buildRustPackage rec {
+      name = "presenterm";
+      version = "0.3.0";
+      # src = /home/gaetan/temp/presenterm;
+      src = fetchFromGitHub {
+        owner = "mfontanini";
+        repo = "presenterm";
+        rev = "refs/tags/v${version}";
+        hash = "sha256-uwLVg/bURz2jLAQZgLujDR2Zewu5pcE9bwEBg/DQ4Iw=";
+      };
+
+      cargoHash = "sha256-nL2EGbNekOHHXffzWa3KuGTjaAVUDJI91WUK3Fw05rQ=";
+
+      checkFlags = [
+        "--skip=execute::test::shell_code_execution"
+      ];
+
+      buildInputs = [libsixel];
+      cargoBuildFlags = ["--features" "sixel"];
+    })
   ];
 }
---
+++ slides/main.md
@@ -0,0 +1,39 @@
+---
+title: "Nixvim: Configure Neovim with the power of Nix"
+---
+
+Nixvim: Configure Neovim with the power of Nix
+---
+
+![](nixvim_logo.png)
+
+<!-- end_slide -->
+What is Nix ?
+---
+
+- A functional programming language
+<!-- pause -->
+- A package manager (`nixpkgs`)
+<!-- pause -->
+- An operating system (NixOS)
+
+<!-- end_slide -->
+Nixpkgs: the nix package repository
+---
+
+- ~80,000 package
+
+![](repology.png)
+
+
+<!-- end_slide -->
+
+
+<!-- end_slide -->
+Thanks for listening !
+---
+
+* 󰖟 glepage.com
+* 󰫑 @glepage@fosstodon.org
+*  @GaetanLepage_
+
