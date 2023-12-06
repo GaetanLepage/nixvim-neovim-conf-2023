79964c7b17039818fc992e2db8a0d242e1d2b03e
Gaetan Lepage <gaetan@glepage.com>
2023-12-06 11:43:52 +0100

    switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -2,72 +2,78 @@
   description = "Nixvim talk @NeovimConf 2023";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     nixpkgs,
     nixvim,
-    flake-utils,
+    flake-parts,
     ...
-  }: let
+  } @ inputs: let
     config = import ./nixvim-config.nix; # import the module directly
   in
-    flake-utils.lib.eachDefaultSystem (system: let
-      nixvimLib = nixvim.lib.${system};
-      pkgs = import nixpkgs {inherit system;};
-      nixvim' = nixvim.legacyPackages.${system};
-      nvim = nixvim'.makeNixvimWithModule {
-        inherit pkgs;
-        module = config;
-      };
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-      presenterm = with pkgs;
-        rustPlatform.buildRustPackage {
-          name = "presenterm";
-          version = "0.3.0";
-          src = fetchFromGitHub {
-            owner = "mfontanini";
-            repo = "presenterm";
-            rev = "33e48fc0086b226bab9c5a8a076022c51303f626";
-            hash = "sha256-gPJkG3fJXc7YIiWNv6TxtCeTqkHX2yoWPsXUbkgeh5o=";
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        presenterm = with pkgs;
+          rustPlatform.buildRustPackage {
+            name = "presenterm";
+            version = "0.3.0";
+            src = fetchFromGitHub {
+              owner = "mfontanini";
+              repo = "presenterm";
+              rev = "33e48fc0086b226bab9c5a8a076022c51303f626";
+              hash = "sha256-gPJkG3fJXc7YIiWNv6TxtCeTqkHX2yoWPsXUbkgeh5o=";
+            };
+
+            cargoHash = "sha256-H+DNQs30twrVEumJqA8GZrcdyiPVEGTLRZFLFfmADK8=";
+
+            checkFlags = [
+              "--skip=execute::test::shell_code_execution"
+            ];
+
+            buildInputs = [libsixel];
+            cargoBuildFlags = ["--features" "sixel"];
           };
 
-          cargoHash = "sha256-H+DNQs30twrVEumJqA8GZrcdyiPVEGTLRZFLFfmADK8=";
-
-          checkFlags = [
-            "--skip=execute::test::shell_code_execution"
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.legacyPackages.${system};
+        nvim = nixvim'.makeNixvimWithModule {
+          inherit pkgs;
+          module = config;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            presenterm
           ];
-
-          buildInputs = [libsixel];
-          cargoBuildFlags = ["--features" "sixel"];
         };
-    in {
-      checks = {
-        # Run `nix flake check .` to verify that your config is not broken
-        default = nixvimLib.check.mkTestDerivationFromNvim {
-          inherit nvim;
-          name = "A nixvim configuration";
+        packages = {
+          # Lets you run `nix run .` to start nixvim
+          default = nvim;
+          slides = pkgs.writeShellApplication {
+            name = "slides";
+            runtimeInputs = [presenterm];
+            text = "presenterm slides.md";
+          };
         };
-      };
 
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          presenterm
-        ];
-      };
+        formatter = pkgs.alejandra;
 
-      packages = {
-        # Lets you run `nix run .` to start nixvim
-        default = nvim;
-        slides = pkgs.writeShellApplication {
-          name = "slides";
-          runtimeInputs = [presenterm];
-          text = "presenterm slides.md";
+        checks = {
+          # Run `nix flake check .` to verify that your config is not broken
+          default = nixvimLib.check.mkTestDerivationFromNvim {
+            inherit nvim;
+            name = "A nixvim configuration";
+          };
         };
       };
-
-      formatter = pkgs.alejandra;
-    });
+    };
 }
